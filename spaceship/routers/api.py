62eb34fdd19cf3223b53b4b203621c6428023cb0
Sashko Vysokovskiy <thettboy11@gmail.com>
2024-06-02 16:24:16 +0300

    add numpy and func sumArray

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,21 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
-@router.get('')
+
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+@router.get("/matrix-multiply")
+def matrix_multiplication() -> dict:
+    mat1 = np.random.rand(10, 10)
+    mat2 = np.random.rand(10, 10)
+
+    result = np.dot(mat1, mat2)
+    return {
+        "first_matrix": mat1.tolist(),
+        "second_matrix": mat2.tolist(),
+        "multiplication_result": result.tolist(),
+    }
+
